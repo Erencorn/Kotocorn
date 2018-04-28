52bccbf8ae1f16126c05cbb49ea9174e330898d9
Erencorn <38808225+Erencorn@users.noreply.github.com>
2018-04-28 12:49:06 +0200

    Now it works for sure.

--- KotoRun.bat
+++ KotoRun.bat
@@ -1,9 +1,9 @@
 @ECHO off
-@TITLE NadekoBot
-CD /D "%~dp0NadekoBot"
+@TITLE Kotocorn
+CD /D "C:\Users\jayd\OneDrive\Development\Kotocorn"
 dotnet restore
 dotnet build --configuration Release
-CD /D "%~dp0NadekoBot\src\NadekoBot"
+CD /D "C:\Users\jayd\OneDrive\Development\Kotocorn\src\NadekoBot"
 dotnet run
 ECHO NadekoBot has been succesfully stopped, press any key to close this window.
 TITLE NadekoBot - Stopped
